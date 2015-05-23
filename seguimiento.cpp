29a71f5e5f77c0760b46f5a7be7f7a7ebeee3359
jrueg <jesus.rueg@gmail.com>
2015-05-24 01:45:23 +0200

    Ajuste resolución y límites

--- seguimiento.cpp
+++ seguimiento.cpp
@@ -6,20 +6,20 @@
 #include <raspicam/raspicam_cv.h>
 #include <time.h>
 
-//#define activa_gui
+#define activa_gui
 
 using namespace cv;
 //initial min and max HSV filter values.
 //these will be changed using trackbars
-int H_MIN = 10;
-int H_MAX = 42;
-int S_MIN = 118;
+int H_MIN = 0;
+int H_MAX = 256;
+int S_MIN = 0;
 int S_MAX = 256;
-int V_MIN = 68;
+int V_MIN = 0;
 int V_MAX = 256;
 //default capture width and height
-const int FRAME_WIDTH = 640;
-const int FRAME_HEIGHT = 480;
+const int FRAME_WIDTH = 320;
+const int FRAME_HEIGHT = 240;
 //max number of objects to be detected in frame
 const int MAX_NUM_OBJECTS=50;
 //minimum and maximum object area
@@ -33,7 +33,10 @@ const string windowName1 = "HSV Image";
 const string windowName2 = "Thresholded Image";
 const string windowName3 = "After Morphological Operations";
 const string trackbarWindowName = "Trackbars";
+void on_trackbar(int, void*)
+{//Esta función se ejecuta cuando cambia la posición de un trackbar
 
+}
 #endif
 
 string intToString(int number){
