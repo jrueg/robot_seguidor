b34cf7f4c52e5af1e01fe98331a8d2898ef84e8f
jrueg <jesus.rueg@gmail.com>
2015-07-02 12:07:33 +0200

    Cambios Script Startup

--- startup.sh
+++ startup.sh
@@ -1,5 +1,4 @@
-echo Descargando ultima version desde Github
-git pull
+
 echo Compilando programa...
 g++ -std=c++0x -o robot *.cpp -I/usr/local/include/ -lraspicam -lraspicam_cv -lrt -lmmal -lmmal_core -lmmal_util -lopencv_core -lopencv_highgui -lopencv_imgproc -L/opt/vc/lib -lpthread -lwiringPi
 echo Fin compilado.
